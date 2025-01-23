3322a46344dff8dc986abcdd58081e381b39ed9d
Ratna Sharma Bhardwaj <ratnasharma296@gmail.com>
2025-01-24 01:30:53 +0200

    error handling

--- Source/main/default/lwc/feedbackForm/feedbackForm.js
+++ Source/main/default/lwc/feedbackForm/feedbackForm.js
@@ -3,16 +3,16 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class FeedbackForm extends LightningElement {
 
-     // Track the record id and whether the record is saved successfully
-     @track recordId;
-     @track isRecordSaved = false;
+    // Track the record id and whether the record is saved successfully
+    @track recordId;
+    @track isRecordSaved = false;
 
-    handleError(event){
+    handleError(event) {
         const evt = new ShowToastEvent({
             title: 'Error!',
             message: event.detail.detail,
             variant: 'error',
-            mode:'dismissable'
+            mode: 'dismissable'
         });
         this.dispatchEvent(evt);
 
@@ -25,28 +25,28 @@ export default class FeedbackForm extends LightningElement {
         this.isRecordSaved = true;
 
         const even = new ShowToastEvent({
-            title: 'Success!',
-            message: 'Record created!',
+            title: 'Feedback is created!',
+            message: 'You can now upload multiple supporting attachments.',
             variant: 'success'
         });
-        
+
         this.dispatchEvent(even);
     }
-    
+
     resetForm() {
-        const inputFields = this.template.querySelectorAll(
-            'lightning-input-field'
-        );
+        const inputFields = this.template.querySelectorAll('lightning-input-field');
         if (inputFields) {
             inputFields.forEach(field => {
                 field.reset();
             });
         }
-     }
+    }
 
-     // Handle file upload completion
+    // Handle file upload completion
     handleUploadFinished(event) {
+        this.isRecordSaved = false;
         const uploadedFiles = event.detail.files;
-        this.showToast('Success', `${uploadedFiles.length} file(s) uploaded successfully!`, 'success');
+        this.ShowToastEvent('Success', `${uploadedFiles.length} file(s) uploaded successfully!`, 'success');
+        this.dispatchEvent(evt);
     }
 }
